8e9cc6750472abaec14e2320ffb98decc4e48395
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-10-16 13:52:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5ClientHub::Application.config.secret_token = '36c36050dff18f4eaf2690ee6584579dd5a1300b6cd8d04143f1681eab28296f1c2ba3f435ce51d25cb644327a04588332bec0f031b7c703ad3214e68f00502a'
