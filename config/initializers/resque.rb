fb89f0ec010b789ef0e6669f2d47e1bf3c6299ad
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-12 11:11:33 -0800

    s/NERD_HTTP_USER/RESQUE_HTTP_USER

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,8 +5,8 @@ Resque.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
 Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
 
 # Require HTTP Basic Auth to view dashboard
-if ENV["NERD_HTTP_USER"] && ENV["NERD_HTTP_PASSWORD"]
+if ENV["RESQUE_HTTP_USER"] && ENV["RESQUE_HTTP_PASSWORD"]
   Resque::Server.use(Rack::Auth::Basic) do |user, password|
-    user == ENV["NERD_HTTP_USER"] and password == ENV["NERD_HTTP_PASSWORD"]
+    user == ENV["RESQUE_HTTP_USER"] and password == ENV["RESQUE_HTTP_PASSWORD"]
   end
 end
