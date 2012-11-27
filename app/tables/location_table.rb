abf4e406844be6d7cb955cb5a16961cf20a3beb0
Bookis Smuin <vegan.bookis@gmail.com>
2012-11-27 11:42:07 -0800

    clean up drag and drop

--- app/tables/location_table.rb
+++ app/tables/location_table.rb
@@ -1,6 +1,6 @@
 class LocationTable < TableCloth::Base
   column :name, :heroku_url
   action {|location, h| h.link_to "View", location.heroku_url, class: "btn" }
-  action {|location, h| h.link_to "Edit", h.location_path(location), class: "btn" }
+  action {|location, h| h.link_to "Pages", h.location_path(location), class: "btn" }
   action {|location, h| h.link_to "Deploy", h.deploy_location_path(location), method: :post, class: "btn"}
 end
