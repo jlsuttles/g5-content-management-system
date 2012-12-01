91b35aedeefa75ce7934c3c33461884d00efb172
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-30 16:41:15 -0800

    adds some comments

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,11 +1,14 @@
 module PagesHelper
   
   def preview(location, page)
+    # grab the layout
     html = Nokogiri.parse(location.site_template.layout.html)
+    # put widgets in their section in the layout
     page.all_widgets.group_by(&:section).each do |section, widgets|
       html_section = html.at_css(".#{section},#{section}")
       html_section.inner_html = widgets.map(&:html).join if html_section
     end
+    # return the modified layout
     html.to_html
   end
 end
