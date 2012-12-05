3bf46282e49d9526345217ed813174706401e491
Bookis Smuin <vegan.bookis@gmail.com>
2012-12-05 12:23:26 -0800

    remove compile buttons

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,18 +14,4 @@ class LocationsController < ApplicationController
     redirect_to locations_path, notice: "Deploying location #{@location.heroku_url}."
   end
   
-  def compile
-    @location = Location.find(params[:id])
-    create_compiled_folder
-    redirect_to :back, notice: "Successfully compiled #{@location.name}."
-  end
-  
-  def create_compiled_folder
-    @location.create_root_directory
-    @location.pages.each do |page|
-      @page = page
-      File.open(@page.compiled_file_path, 'w') { |file| file << render_to_string("/pages/preview", layout: false) }
-    end
-  end
-  helper_method :create_compiled_folder
 end
--- lib/tasks/seed_client.rake
+++ lib/tasks/seed_client.rake
@@ -1,3 +1,4 @@
 task "seed_client" => :environment do
+  # http://g5-hub.herokuapp.com/clients/9
   G5ClientHub::ClientReader.perform(ENV["G5_CLIENT_UID"])
 end
