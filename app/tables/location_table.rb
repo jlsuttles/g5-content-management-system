bd3d1a7ba7c6f8692dbab250f051290f43bfb6ad
Bookis Smuin <vegan.bookis@gmail.com>
2012-12-05 12:23:26 -0800

    remove compile buttons

--- app/tables/location_table.rb
+++ app/tables/location_table.rb
@@ -2,6 +2,5 @@ class LocationTable < TableCloth::Base
   column :name, :heroku_url
   action {|location, h| h.link_to "View", location.heroku_url, class: "btn" }
   action {|location, h| h.link_to "Pages", h.location_path(location), class: "btn" }
-  action {|location, h| h.link_to "Compile", h.compile_location_path(location), method: :post, class: "btn"}
   action {|location, h| h.link_to "Deploy", h.deploy_location_path(location), method: :post, class: "btn"}
 end
