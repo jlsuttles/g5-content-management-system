c1b6e8c026a9bec18b8def27dd3acaafdec10633
Bookis Smuin <vegan.bookis@gmail.com>
2012-12-11 11:38:17 -0800

    make index.html deploy as home page

--- app/workers/location_deployer.rb
+++ app/workers/location_deployer.rb
@@ -11,19 +11,20 @@ class LocationDeployer
       @location.clean_up
     end
   end
-  
+
   def self.compile_pages(location)
     location.create_root_directory
     add_page_at_path(location.homepage, location, location.compiled_site_path + "index.html")
     location.pages.each do |page|
+      puts page.name
       add_page_at_path(page, location, page.compiled_file_path)
     end
   end
-  
+
   def self.add_page_at_path(page, location, path)
     File.open(path, 'w') do |file|
       file << LocationsController.new.render_to_string("/pages/preview", layout: false, :locals => {:page => page, location: location})
     end
   end
-  
+
 end
