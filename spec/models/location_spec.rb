5ba799cf384587dfda4fb674f3925a0811319433
Bookis Smuin <vegan.bookis@gmail.com>
2012-12-12 13:16:58 -0800

    remove whitespace?

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -5,19 +5,19 @@ describe Location do
   it "should be valid" do
     Location.new.should be_valid
   end
-  
+
   it "adds a page on creation" do
     expect { location }.to change(Page, :count).by(2)
   end
-  
+
   it "doesn't count template as a page" do
     template = Page.create(location_id: location.id, name: "Template", template: true)
     location.pages.should_not include template
   end
-  
+
   it "has a template as a page" do
     location.site_template.should_not be_blank
   end
-  
+
 end
 
