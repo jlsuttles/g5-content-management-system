247c6d40a53b15cf8c6a2c55067402a61847137b
Bookis Smuin <vegan.bookis@gmail.com>
2012-12-12 13:54:50 -0800

    add optional placeholder images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def image_tag_or_placeholder(src, options={})
+    if src.blank?
+      src = "http://placehold.it/#{options[:width] || 100 }x#{options[:height] || 100}"
+    end
+    image_tag(src, options)
+  end
 end
