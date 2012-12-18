5f22084e4264879fdd1ddba7385ab7a1d5c46762
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-17 19:19:15 -0800

    removes compiled pages stylesheets from asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ G5ClientHub::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( compiled_pages.js compiled_pages.css )
+  config.assets.precompile += %w( compiled_pages.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
