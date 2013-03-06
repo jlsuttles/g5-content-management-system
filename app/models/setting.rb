8d26a9acbeb3d2820588aab8d9ff134ec608658f
Jessica Lynn Suttles & Michael Mitchell <g5dev+jls+mj@g5searchmarketing.com>
2013-03-06 14:19:49 -0800

    sets lead widget submission_url value automagically based on our own heroku app name

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,9 +1,14 @@
 class Setting < ActiveRecord::Base
   include AssociationToMethod
+
+  attr_accessible :categories, :component_id, :component_type, :name
+
+  serialize :categories, Array
+
   belongs_to :component, polymorphic: true
   has_many :widget_attributes
+
   alias_method :dynamic_association, :widget_attributes
-  attr_accessible :categories, :component_id, :component_type, :name
-  serialize :categories, Array
+
   validates :component, :name, presence: true
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,29 +1,44 @@
 class Widget < ActiveRecord::Base
   include AssociationToMethod
+
   WIDGET_GARDEN_URL = "http://g5-widget-garden.herokuapp.com"
-  attr_accessible :page_id, :section, :position, :url, :name, :stylesheets, :javascripts, :html, :thumbnail, :edit_form_html, :widget_attributes_attributes
-  has_many :settings, as: :component, after_add: :define_dynamic_association_method
-  alias_attribute :dynamic_association, :settings
-  has_many :widget_attributes, through: :settings
-  accepts_nested_attributes_for :widget_attributes
+
+  attr_accessible :page_id, :section, :position, :url, :name, :stylesheets,
+                  :javascripts, :html, :thumbnail, :edit_form_html,
+                  :widget_attributes_attributes
+
   serialize :stylesheets, Array
   serialize :javascripts, Array
+
   belongs_to :page
+  has_many :settings, as: :component, after_add: :define_dynamic_association_method
+  has_many :widget_attributes, through: :settings
+
+  accepts_nested_attributes_for :widget_attributes
+
+  alias_attribute :dynamic_association, :settings
+
   before_create :assign_attributes_from_url
+
   validates :url, presence: true
+
   scope :in_section, lambda { |section| where(section: section) }
-  
+
   def self.all_remote
     components = G5HentryConsumer::HG5Component.parse(WIDGET_GARDEN_URL)
     components.map do |component|
       new(url: component.uid, name: component.name.first, thumbnail: component.thumbnail.first)
     end
   end
-  
+
   def liquidized_html
     Liquid::Template.parse(CGI::unescape(self.html)).render("widget" => self)
   end
-  
+
+  def kind_of_widget?(kind)
+    name == kind
+  end
+
   private
 
   def assign_attributes_from_url
@@ -43,7 +58,7 @@ class Widget < ActiveRecord::Base
   rescue OpenURI::HTTPError => e
     logger.warn e.message
   end
-  
+
   def parse_settings(configs)
     return if configs.blank?
     configs.each do |config|
@@ -53,10 +68,10 @@ class Widget < ActiveRecord::Base
       end
     end
   end
-  
+
   def get_edit_form_html(component)
     url = component.edit_template.try(:first)
     open(url).read if url
   end
-  
+
 end
