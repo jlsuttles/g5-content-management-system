3af434e2dc48a2f06cb259708a38fe6a151677ea
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-18 19:56:19 -0700

    adds another spec for tags controller

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
-describe LocationsController do
+describe TagsController do
   let(:tag) { "sometag" }
 
   describe "#show" do
     it "show action should render show template" do
       get :show, id: tag
       response.should render_template(:show)
+    end
+    it "assigs params[:id] to @tag" do
+      get :show, id: tag
+      assigns(:tag).should eq "sometag"
     end
   end
 end
