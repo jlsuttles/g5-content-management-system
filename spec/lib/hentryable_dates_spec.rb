a2820f1f5077b5c565c03760bbf3d87215813482
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-19 10:31:58 -0700

    drys up hentryable dates spec by moving regex to contants. thanks @jcstringer

--- spec/lib/hentryable_dates_spec.rb
+++ spec/lib/hentryable_dates_spec.rb
@@ -5,35 +5,34 @@ class HentryableDatesTester
 end
 
 describe HentryableDates do
+  COMPUTER_REGEX = /#{Time::DATE_FORMATS[:computer].gsub(/%./, "\\d+")}/
+  HUMAN_REGEX = /#{Time::DATE_FORMATS[:human].gsub(/%./, ".+")}/
+
   let(:tester) { HentryableDatesTester.new }
   before { tester.stub_chain(:model, :created_at).and_return(Time.now) }
   before { tester.stub_chain(:model, :updated_at).and_return(Time.now) }
 
   describe "#created_at_computer_readable" do
     it "is computer readable" do
-      regex = /#{Time::DATE_FORMATS[:computer].gsub(/%./, "\\d+")}/
-      tester.created_at_computer_readable.should match regex
+      tester.created_at_computer_readable.should match COMPUTER_REGEX
     end
   end
 
   describe "#created_at_human_readable" do
     it "is human readable" do
-      regex = /#{Time::DATE_FORMATS[:human].gsub(/%./, ".+")}/
-      tester.created_at_human_readable.should match regex
+      tester.created_at_human_readable.should match HUMAN_REGEX
     end
   end
 
   describe "#updated_at_computer_readable" do
     it "is computer readable" do
-      regex = /#{Time::DATE_FORMATS[:computer].gsub(/%./, "\\d+")}/
-      tester.updated_at_computer_readable.should match regex
+      tester.updated_at_computer_readable.should match COMPUTER_REGEX
     end
   end
 
   describe "#updated_at_human_readable" do
     it "is human readable" do
-      regex = /#{Time::DATE_FORMATS[:human].gsub(/%./, ".+")}/
-      tester.updated_at_human_readable.should match regex
+      tester.updated_at_human_readable.should match HUMAN_REGEX
     end
   end
 end
