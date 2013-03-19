a70e9c6baa7e9b42d512a19dcf0c95d9249e08b8
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-19 14:19:41 -0700

    adds specs to widget entry model

--- spec/models/widget_entry_spec.rb
+++ spec/models/widget_entry_spec.rb
@@ -4,7 +4,37 @@ describe WidgetEntry do
   let(:widget_entry) { Fabricate.build(:widget_entry) }
   let(:widget) { widget_entry.widget }
 
-  it "saves widget html before create" do
-    widget_entry.content.should == widget.html
+  describe "filters" do
+    context "before_create" do
+      it "saves widget html" do
+        widget_entry.content.should == widget.html
+      end
+    end
+  end
+
+  describe "#widget_name" do
+    context "widget is nil" do
+      it "returns nil" do
+        WidgetEntry.new.widget_name.should be_nil
+      end
+    end
+    context "widget is present" do
+      it "returns widget's name" do
+        widget_entry.widget_name.should eq widget.name
+      end
+    end
+  end
+
+  describe "#widget_html" do
+    context "widget is nil" do
+      it "returns nil" do
+        WidgetEntry.new.widget_html.should be_nil
+      end
+    end
+    context "widget is present" do
+      it "returns widget's html" do
+        widget_entry.widget_html.should eq widget.html
+      end
+    end
   end
 end
