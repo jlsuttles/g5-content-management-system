d5ce1f1851462e3a69f2588b5982454f4b0f90bd
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-18 19:56:28 -0700

    adds more specs for hentryable dates

--- spec/lib/hentryable_dates_spec.rb
+++ spec/lib/hentryable_dates_spec.rb
@@ -7,6 +7,7 @@ end
 describe HentryableDates do
   let(:tester) { HentryableDatesTester.new }
   before { tester.stub_chain(:model, :created_at).and_return(Time.now) }
+  before { tester.stub_chain(:model, :updated_at).and_return(Time.now) }
 
   describe "#created_at_computer_readable" do
     it "is computer readable" do
@@ -19,6 +20,20 @@ describe HentryableDates do
     it "is human readable" do
       regex = /#{Time::DATE_FORMATS[:human].gsub(/%./, ".+")}/
       tester.created_at_human_readable.should match regex
+    end
+  end
+
+  describe "#updated_at_computer_readable" do
+    it "is computer readable" do
+      regex = /#{Time::DATE_FORMATS[:computer].gsub(/%./, "\\d+")}/
+      tester.updated_at_computer_readable.should match regex
+    end
+  end
+
+  describe "#updated_at_human_readable" do
+    it "is human readable" do
+      regex = /#{Time::DATE_FORMATS[:human].gsub(/%./, ".+")}/
+      tester.updated_at_human_readable.should match regex
     end
   end
 end
