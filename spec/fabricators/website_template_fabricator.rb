fbaae10640da3f90541b3698d59fa1c7905d0331
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-04-07 17:53:50 -0700

    simplifies website template fabricator

--- spec/fabricators/website_template_fabricator.rb
+++ spec/fabricators/website_template_fabricator.rb
@@ -1,11 +1,6 @@
 Fabricator :website_template do
   name { Faker::Name.name }
-  slug  "slug"
-  title "What a wonderful world"
-  template true
-  web_layout
-  web_theme
-  website
-  aside_widgets(fabricator: :widget, count: 1)
+  title { Faker::Name.name }
+  slug { Faker::Name.name.parameterize }
 end
 
