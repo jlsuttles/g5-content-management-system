bb4e56c8050f8b8cf4fa4bd4098260ec532d8e8d
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-04-08 14:36:03 -0700

    adds more specs for website_template_controller

--- spec/controllers/website_templates_controller_spec.rb
+++ spec/controllers/website_templates_controller_spec.rb
@@ -1,24 +1,60 @@
 require 'spec_helper'
 
 describe WebsiteTemplatesController do
-  let(:location) { Fabricate(:location) }
-  before { WebsiteTemplate.stub(:find) { location.website_template } }
-  describe "#edit" do
-    it "renders the web_templates template" do
-      get :edit, id: 1, location_id: location.urn
-      response.should render_template "web_templates/edit"
-    end
+  let(:location) { Fabricate.build(:location) }
+  let(:website) { Fabricate.build(:website) }
+  let(:website_template) { Fabricate.build(:website_template) }
 
-    it "assigns the location attr" do
-      get :edit, id: 1, location_id: location.urn
+  before do
+    Location.stub(:find_by_urn) { location }
+    WebsiteTemplate.stub(:find) { website_template }
+
+    website.website_template = website_template
+    location.website = website
+  end
+
+  describe "#edit" do
+    it "assigns the location instance variable" do
+      get :edit, location_id: location.urn, id: 1
       assigns(:location).should eq location
+    end
+    it "assigns the web_template instance variable" do
+      get :edit, location_id: location.urn, id: 1
+      assigns(:web_template).should eq website_template
+    end
+    it "renders the layout/builder template" do
+      get :edit, location_id: location.urn, id: 1
+      response.should render_template "layouts/builder"
+    end
+    it "renders the web_templates/edit template" do
+      get :edit, location_id: location.urn, id: 1
+      response.should render_template "web_templates/edit"
     end
   end
 
   describe "#update" do
+    it "assigns the location instance variable" do
+      get :update, location_id: location.urn, id: 1
+      assigns(:location).should eq location
+    end
+    it "assigns the web_template instance variable" do
+      get :update, location_id: location.urn, id: 1
+      assigns(:web_template).should eq website_template
+    end
     it "updates the object" do
-      put :update, id: 1, location_id: location.urn, website_template: {name: "Some Template"}
-      location.website_template.reload.name.should eq "Some Template"
+      put :update, location_id: location.urn, id: 1,
+        website_template: { name: "Some Template" }
+      website_template.reload.name.should eq "Some Template"
+    end
+    it "when update succeeds redirects to location" do
+      website_template.stub(:update_attributes).and_return(true)
+      get :update, location_id: location.urn, id: 1
+      response.should redirect_to location
+    end
+    it "when update fails renders web_templates/edit template" do
+      website_template.stub(:update_attributes).and_return(false)
+      get :update, location_id: location.urn, id: 1
+      response.should render_template "layouts/builder"
     end
   end
 
