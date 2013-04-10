e886b94bba27135f35390bc9d1ad5758821b4de9
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-04-09 21:46:01 -0700

    adds spec for has_setting_navigation concern

--- app/models/concerns/has_setting_navigation.rb
+++ app/models/concerns/has_setting_navigation.rb
@@ -3,15 +3,15 @@ require_dependency "hash_with_to_liquid"
 module HasSettingNavigation
   extend ActiveSupport::Concern
 
-  def set_setting_navigation
-    setting_navigation.update_attribute(:value, generate_setting_navigation)
-  end
-
   def setting_navigation
     settings.find_or_initialize_by_name("navigation")
   end
 
-  def generate_setting_navigation
+  def set_setting_navigation
+    setting_navigation.update_attribute(:value, web_page_templates_to_hashes)
+  end
+
+  def web_page_templates_to_hashes
     web_page_templates.decorate.map do |web_page_template|
       hash = HashWithToLiquid.new
       hash["display"] = web_page_template.display
---
+++ spec/models/concerns/has_setting_navigation_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+
+shared_examples_for HasSettingNavigation do
+  let(:described_instance) { described_class.new }
+
+  describe "#setting_navigation" do
+    context "navigation setting exists" do
+      let(:navigation) { described_instance.settings.build(name: "navigation") }
+      it "finds setting with name navigation" do
+        navigation.save(validate: false)
+        described_instance.setting_navigation.should eq navigation
+      end
+    end
+    context "navigation setting does not exist" do
+      it "initializes setting with name navigation" do
+        described_instance.setting_navigation.new_record?.should be_true
+      end
+    end
+  end
+
+  describe "#web_page_templates_to_hashes" do
+    context "has no web pages" do
+      it "returns an empty array" do
+        described_instance.web_page_templates_to_hashes.should eq []
+      end
+    end
+    context "has web pages" do
+      let(:web_page_template) { Fabricate.build(:web_page_template) }
+      before do
+        described_instance.web_page_templates << web_page_template
+        described_instance.save(validate: false)
+      end
+      it "returns hashes with to liquid defined" do
+        described_instance.web_page_templates_to_hashes.sample.should be_a(HashWithToLiquid)
+      end
+      it "hashes have key 'display'" do
+        described_instance.web_page_templates_to_hashes.sample["display"].should_not be_nil
+      end
+      it "hashes have key 'title'" do
+        described_instance.web_page_templates_to_hashes.sample["title"].should_not be_nil
+      end
+      it "hashes have key 'url'" do
+        described_instance.web_page_templates_to_hashes.sample["url"].should_not be_nil
+      end
+    end
+  end
+end
+
+describe Website do
+  it_behaves_like HasSettingNavigation
+end
