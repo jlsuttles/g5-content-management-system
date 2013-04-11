ca19a6a1a430dbde03a98cddead9484e177d293c
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-04-11 10:49:37 -0700

    updates schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -121,6 +121,15 @@ ActiveRecord::Schema.define(:version => 20130319030042) do
 
   add_index "themes", ["page_id"], :name => "index_themes_on_page_id"
 
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
   create_table "widget_attributes", :force => true do |t|
     t.string   "name"
     t.string   "value"
