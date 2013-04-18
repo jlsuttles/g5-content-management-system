e972205e94039a2f258c796f28f0ad9ee0e1fa6d
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-04-18 11:39:05 -0700

    adds default garden urls for layouts and themes

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -2,4 +2,6 @@ ENV["HEROKU_APP_NAME"] ||= "g5-ch-default"
 ENV["LEADS_SERVICE_HEROKU_APP_NAME"] ||= ENV["HEROKU_APP_NAME"].gsub(/-ch-/, "-cls-")[0..29]
 ENV["LEADS_SERVICE_HEROKU_URL"] ||= "http://#{ENV["LEADS_SERVICE_HEROKU_APP_NAME"]}.herokuapp.com/leads"
 ENV['WIDGET_GARDEN_URL'] ||= "http://g5-widget-garden.herokuapp.com"
+ENV['THEME_GARDEN_URL'] ||= "http://g5-theme-garden.herokuapp.com"
+ENV['LAYOUT_GARDEN_URL'] ||= "http://g5-layout-garden.herokuapp.com"
 
