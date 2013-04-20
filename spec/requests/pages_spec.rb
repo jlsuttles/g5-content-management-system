f570a38486906009c377ae2f67b3914c71b30381
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-04-19 18:20:23 -0700

    trying to fix specs on circleci

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,10 +7,10 @@ describe "pages requests", js: true do
   end
 
   describe "#index" do
-    before do
-      visit location_path(@location)
-    end
     describe "create new page link" do
+      before do
+        visit location_path(@location)
+      end
       it "should go to the new page page" do
         click_link "Create New Page"
         current_path.should eq new_location_page_path(@location)
@@ -18,15 +18,14 @@ describe "pages requests", js: true do
     end
     describe "toggle page disabled" do
       it "should disable the page when i click the toggle" do
+        visit location_path(@location)
         first(".switch").first(".switch-left").click
-        visit(current_path)
         first(".switch").should have_css(".switch-off")
       end
       it "should enable the page when i click the toggle" do
         @location.pages.first.update_attribute(:disabled, true)
-        visit(current_path)
+        visit location_path(@location)
         first(".switch").first(".switch-left").click
-        visit(current_path)
         first(".switch").should have_css(".switch-on")
       end
     end
