4ae2a43ba581fb3cc7a632953700c740d10ee914
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-05-20 13:56:01 -0700

    adds specs for website_template default header and footer widgets

--- spec/models/website_template_spec.rb
+++ spec/models/website_template_spec.rb
@@ -21,4 +21,36 @@ describe WebsiteTemplate do
       website_template.javascripts.should be_kind_of(Array)
     end
   end
+
+  describe "default header widgets" do
+    before do
+      WebsiteTemplate.any_instance.unstub(:create_default_header_widgets)
+      Widget.stub(build_widget_url: "spec/support/widget.html")
+      WebsiteTemplate.any_instance.stub(default_header_widgets: ["storage-list"])
+      @website_template = Fabricate(:website_template)
+    end
+    it "builds default header widgets" do
+      @website_template.widgets.map(&:name).should include("Storage List")
+    end
+    it "assigns the widgets to the 'header' section" do
+      @website_template.header_widgets.count.should ==
+        @website_template.default_header_widgets.length
+    end
+  end
+
+  describe "default footer widgets" do
+    before do
+      WebsiteTemplate.any_instance.unstub(:create_default_footer_widgets)
+      Widget.stub(build_widget_url: "spec/support/widget.html")
+      WebsiteTemplate.any_instance.stub(default_footer_widgets: ["storage-list"])
+      @website_template = Fabricate(:website_template)
+    end
+    it "builds default footer widgets" do
+      @website_template.widgets.map(&:name).should include("Storage List")
+    end
+    it "assigns the widgets to the 'footer' section" do
+      @website_template.footer_widgets.count.should ==
+        @website_template.default_footer_widgets.length
+    end
+  end
 end
