5afec2433d07161d40c65789569fe8d57fd75c85
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-05-21 15:24:58 -0700

    creates website_templates with default widgets

--- lib/website_template_with_default_widgets.rb
+++ lib/website_template_with_default_widgets.rb
@@ -1,11 +1,11 @@
-class WebsiteTemplateWithDefaultWidgets do
-  DEFAULTS = [
+class WebsiteTemplateWithDefaultWidgets
+  DEFAULTS = {
     "drop-target-logo" => ["logo"],
     "drop-target-phone" => ["phone"],
     "drop-target-btn" => ["button"],
     "drop-target-nav" => ["simple-nav"],
     "footer" => ["simple-nav", "hcard", "hours"]
-  ]
+  }
 
   def initialize(website_template)
     @website_template = website_template
@@ -18,16 +18,16 @@ class WebsiteTemplateWithDefaultWidgets do
   end
 
   def create_default_widgets
-    DEFAULTS.each_pair do |drop_target, widgets|
-      widgets.each do |widget|
-        create_default_widget(widget, drop_target)
+    DEFAULTS.each_pair do |drop_target, widget_names|
+      widget_names.each do |widget_name|
+        create_default_widget(widget_name, drop_target)
       end
     end
   end
 
   def create_default_widget(widget_name, drop_target)
     @website_template.widgets.create(
-      url: Widget.build_widget_url(widget_name)
+      url: Widget.build_widget_url(widget_name),
       section: drop_target,
       removeable: false
     )
