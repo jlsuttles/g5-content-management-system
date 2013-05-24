eb8059a7c0bd10bb0d773f336ef24c11772b6fe0
Jessica Dillon & Jessica Lynn Suttles <g5dev+jd+jls@g5searchmarketing.com>
2013-05-24 11:05:03 -0600

    Adds lib_javascripts to widget model and markup

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -25,6 +25,7 @@ describe Widget do
     it { widget.stylesheets.should have(1).thing }
     it { widget.edit_javascript.should eq "http://g5-widget-garden.herokuapp.com/static/components/storage-list/javascripts/edit.js" }
     it { widget.show_javascript.should eq "http://g5-widget-garden.herokuapp.com/static/components/storage-list/javascripts/show.js" }
+    it { widget.lib_javascripts.should eq ["http://g5-widget-garden.herokuapp.com/javascripts/libs/lib.js"] }
     it { widget.edit_form_html.should eq "I'm an edit form!" }
     it { widget.html.should include "I'm a show page!" }
     it { widget.thumbnail.should eq "http://g5-widget-garden.herokuapp.com/static/components/storage-list/images/thumbnail.png"}
