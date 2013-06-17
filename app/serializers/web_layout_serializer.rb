88c7f3975a5e66476371d172c3811936b6a2b301
Jessica Dillon & Jessica Lynn Suttles <g5dev+jd+jls@g5searchmarketing.com>
2013-06-17 15:25:58 -0700

    correctly names WebLayoutSerializer

--- app/serializers/web_layout_serializer.rb
+++ app/serializers/web_layout_serializer.rb
@@ -1,4 +1,4 @@
-class WebsiteTemplateSerializer < ActiveModel::Serializer
+class WebLayoutSerializer < ActiveModel::Serializer
   attributes  :created_at,
               :updated_at,
               :html,
