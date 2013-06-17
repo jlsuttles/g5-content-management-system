f0805e23733b9dbfae77425aa707aef3d9f48b42
Jessica Dillon & Jessica Lynn Suttles <g5dev+jd+jls@g5searchmarketing.com>
2013-06-17 15:54:53 -0700

    adds Api::V1::WebLayoutsController#index and makes remote layouts available

--- app/controllers/api/v1/web_layouts_controller.rb
+++ app/controllers/api/v1/web_layouts_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::WebLayoutsController < Api::V1::ApplicationController
+  def index
+    render json: WebLayout.all_remote
+  end
+
   def show
     render json: WebLayout.find(params[:id])
   end
