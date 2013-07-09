772ab5b68f219af00a7158944ac7c201aba51c19
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-07-09 12:51:54 -0700

    uses remote component serializers in controllers

--- app/controllers/api/v1/remote_widgets_controller.rb
+++ app/controllers/api/v1/remote_widgets_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RemoteWidgetsController < Api::V1::ApplicationController
   def index
-    render json: Widget.all_remote
+    render json: Widget.all_remote, each_serializer: RemoteWidgetSerializer
   end
 end
