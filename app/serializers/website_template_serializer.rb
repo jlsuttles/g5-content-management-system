debb96d6c248ec82818c0772b339204c3f1c1200
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-07-20 12:47:47 -0700

    adds specific widgets from sections to website_template serializer

--- app/serializers/website_template_serializer.rb
+++ app/serializers/website_template_serializer.rb
@@ -3,6 +3,12 @@ class WebsiteTemplateSerializer < ActiveModel::Serializer
               :web_layout_id,
               :web_theme_id,
               :widget_ids,
+              :logo_widget_ids,
+              :phone_widget_ids,
+              :btn_widget_ids,
+              :nav_widget_ids,
+              :aside_widget_ids,
+              :footer_widget_ids,
               :location_id,
               :website_urn,
               :web_home_template_id
