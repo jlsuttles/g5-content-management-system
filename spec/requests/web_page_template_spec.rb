84fb4d020a8b6ac318d41340f0b49d608214e223
Jessica Dillon <jessicard@mac.com>
2013-07-23 13:04:19 -0400

    fixes tests

--- spec/requests/web_page_template_spec.rb
+++ spec/requests/web_page_template_spec.rb
@@ -20,16 +20,16 @@ describe "web_page_templates requests", js: true do
     describe "toggle page disabled" do
       it "should disable the page when i click the toggle" do
         visit website_path(@website)
-        first(".webpagetemplate").first(".flip-btn").click
-        first(".webpagetemplate").first(".switch").first(".switch-left").click
-        first(".webpagetemplate").first(".switch").should have_css(".switch-success")
+        find(".webhometemplate").first(".flip-btn").click
+        find(".webhometemplate").first(".switch").first(".switch-left").click
+        find(".webhometemplate").first(".switch").should have_css(".switch-success")
       end
       it "should enable the page when i click the toggle" do
         @website.web_home_template.update_attribute(:disabled, true)
         visit website_path(@website)
-        first(".webpagetemplate").first(".flip-btn").click
-        first(".webpagetemplate").first(".switch").first(".switch-left").click
-        first(".webpagetemplate .switch").should have_css(".switch-danger")
+        find(".webhometemplate").first(".flip-btn").click
+        find(".webhometemplate").first(".switch").first(".switch-left").click
+        find(".webhometemplate .switch").should have_css(".switch-danger")
       end
     end
   end
