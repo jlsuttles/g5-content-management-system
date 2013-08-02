9a450685762fdc796859e123602b6d561a97dd9a
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-08-02 16:10:04 -0700

    uses new path

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,5 +1,6 @@
 class WidgetsController < ApplicationController
   respond_to :html, :json, :js
+
   def edit
     @widget = Widget.find(params[:id])
     @form_html = @widget.edit_form_html_rendered
@@ -14,7 +15,7 @@ class WidgetsController < ApplicationController
     if @widget.update_attributes(params[:widget])
       respond_with(@widget) do |format|
         format.json { render json: @widget, status: 204}
-        format.html { redirect_to website_web_page_template_path(@widget.web_template) }
+        format.html { redirect_to web_template_path(@widget.web_template) }
       end
     else
       respond_with do |format|
