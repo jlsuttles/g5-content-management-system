16c5997f96b103c97cc717fed53bea4f7017e3f2
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-10-31 14:11:41 -0700

    Use defaults for Capybara integration specs

--- spec/requests/websites_spec.rb
+++ spec/requests/websites_spec.rb
@@ -1,28 +1,43 @@
 require "spec_helper"
 
-describe "website requests", js: true, vcr: VCR_OPTIONS do
+LOCATION_SELECTOR = ".faux-table .faux-table-row:first-of-type .buttons"
+PAGE_SELECTOR = ".cards .card:first-of-type"
+
+describe "integration /ember/#/website/4", js: true, vcr: VCR_OPTIONS do
   before do
-    @client = Fabricate(:client)
-    @location = Fabricate(:location)
-    @website = Fabricate(:website, location_id: @location.id)
+    ClientReader.perform(ENV["G5_CLIENT_UID"])
+    WebsiteSeederJob.perform
+
+    @client = Client.first
+    @location = Location.first
+    @web_home_template = @location.website.web_home_template
+
+    visit root_path
+
+    within LOCATION_SELECTOR do
+      click_link "Edit"
+    end
   end
 
-  context "#show" do
-    before do
-      visit root_path
+  it "Displays client, location, and page names" do
+    Capybara.default_wait_time = 5
+    within "header" do
+      # CSS upcases these names, so we upcase
+      page.should have_content @client.name.upcase
+      page.should have_content @location.name.upcase
+    end
+
+    within PAGE_SELECTOR do
+      page.should have_content @web_home_template.name.upcase
+    end
+  end
+
+  it "'Edit' link goes to Ember App", vcr_options: { record: :new } do
+    within PAGE_SELECTOR do
       click_link "Edit"
     end
 
-    it "should have content" do
-      page.should have_content @client.name.upcase
-      page.should have_content @website.name.upcase
-    end
-
-    it "goes to edit when I click edit link" do
-      within ".site-pages .cards .card .flipper .front .card-body .buttons" do
-        click_link "Edit"
-      end
-      current_path.should eq "/website/#{@website.id}"
-    end
+    Capybara.default_wait_time = 5
+    current_path.should eq "/location/#{@location.id}/home/#{@web_home_template.id}"
   end
 end
