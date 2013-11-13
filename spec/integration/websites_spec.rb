4520f252563ac7616ceb46c9574e542e9e88b3af
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-11-12 23:44:21 -0800

    Speed up websites integration spec setup

--- spec/integration/websites_spec.rb
+++ spec/integration/websites_spec.rb
@@ -1,45 +1,57 @@
 require "spec_helper"
 
 LOCATION_SELECTOR = ".faux-table .faux-table-row:first-of-type .buttons"
-PAGE_SELECTOR = ".cards .card:first-of-type"
+HOME_SELECTOR = ".cards .card:first-of-type"
+PAGE_SELECTOR = ".cards .card:last-of-type"
 
 describe "Integration '/website/:id'", js: true, vcr: VCR_OPTIONS do
   describe "Lists all web templates" do
     before do
-      ClientReader.perform(ENV["G5_CLIENT_UID"])
-      WebsiteSeederJob.perform
+      # Setup Rails database
+      @client = Fabricate(:client)
+      @location = Fabricate(:location)
+      @instructions = YAML.load_file("#{Rails.root}/spec/support/website_instructions/example.yml")
+      @website = WebsiteSeeder.new(@location, @instructions["website"]).seed
+      @web_home_template = @website.web_home_template
+      @web_page_template = @website.web_page_templates.first
 
-      @client = Client.first
-      @location = Location.first
-      @web_home_template = @location.website.web_home_template
-
-      visit root_path
-
+      # Navigate to Ember application page
+      # TODO: Get deep linking working for testing.
+      visit "/"
       within LOCATION_SELECTOR do
         click_link "Edit"
       end
     end
 
     it "Displays client, location, and page names" do
-      Capybara.default_wait_time = 5
       within "header" do
-        # CSS upcases these names, so we upcase
         page.should have_content @client.name.upcase
         page.should have_content @location.name.upcase
       end
 
-      within PAGE_SELECTOR do
+      within HOME_SELECTOR do
         page.should have_content @web_home_template.name.upcase
+      end
+
+      within PAGE_SELECTOR do
+        page.should have_content @web_page_template.name.upcase
       end
     end
 
-    it "'Edit' link goes to Ember App", vcr_options: { record: :new } do
+    it "Home 'Edit' link goes to '/location/:location_id/home/:home_id'" do
+      within HOME_SELECTOR do
+        click_link "Edit"
+      end
+
+      current_path.should eq "/location/#{@location.id}/home/#{@web_home_template.id}"
+    end
+
+    it "Page 'Edit' link goes to '/location/:location_id/page/:page_id'" do
       within PAGE_SELECTOR do
         click_link "Edit"
       end
 
-      Capybara.default_wait_time = 5
-      current_path.should eq "/location/#{@location.id}/home/#{@web_home_template.id}"
+      current_path.should eq "/location/#{@location.id}/page/#{@web_page_template.id}"
     end
   end
 end
