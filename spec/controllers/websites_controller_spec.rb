9e1889e54d601a1731d8003d4c17a314e4f4ddbb
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-11-13 16:46:14 -0800

    Remove old Rails website#show route, controller, views and specs

--- spec/controllers/websites_controller_spec.rb
+++ spec/controllers/websites_controller_spec.rb
@@ -7,24 +7,6 @@ describe WebsitesController do
     Website.stub(:find).and_return(website)
   end
 
-  describe "#show" do
-    before do
-      get :show, id: 1
-    end
-    it "show action should render show template" do
-      response.should render_template(:show)
-    end
-    it "assigns website" do
-      assigns(:website).should eq website
-    end
-    it "assigns website_template" do
-      assigns(:website_template).should eq website.website_template
-    end
-    it "assings web_page_templates only the navigateable templates orderd by created at" do
-      assigns(:web_page_templates).should eq website.web_templates.navigateable.created_at_asc
-    end
-  end
-
   describe "#deploy" do
     it "redirects to root" do
       Resque.stub(:enqueue).and_return(true)
