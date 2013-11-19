1b498fcd681a7c067d7aeed1302a027b36342211
jessicard <jessicard@mac.com>
2013-11-18 17:13:46 -0800

    adds pending website controller spec

--- spec/controllers/api/v1/websites_controller_spec.rb
+++ spec/controllers/api/v1/websites_controller_spec.rb
@@ -3,6 +3,18 @@ require "spec_helper"
 describe Api::V1::WebsitesController do
   let(:website) { Fabricate(:website) }
 
+  describe "#index" do
+    before do
+      WebsiteSerializer.new(website)
+    end
+
+    it "renders websites as json" do
+      get :index
+      expect(response.status).to eq 200
+      pending("response.body JSON equals Website.all (after ran through the serializer and as JSON)")
+    end
+  end
+
   describe "#show" do
     it "finds website" do
       Website.should_receive(:find).with(website.id.to_s).once
