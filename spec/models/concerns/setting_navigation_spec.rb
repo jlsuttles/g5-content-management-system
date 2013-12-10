f2e3d29a495ba26abc0b812a4c995b7962c9b947
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-12-10 15:36:56 -0800

    Change how website and widget navigation settings are combined
    Use keys to access hash instead of indicies to access array.

--- spec/models/concerns/setting_navigation_spec.rb
+++ spec/models/concerns/setting_navigation_spec.rb
@@ -4,17 +4,20 @@ shared_examples_for SettingNavigation do
   let(:described_instance) { described_class.new }
 
   describe "When display changes on widget" do
-    let(:website_value) {[
-        {"display"=>true, "title"=>"Homepage", "url"=>"/homepage"},
-        {"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
-      ]}
-    let(:widget_value) {
-      {"0"=>{"display"=>false}, "1"=>{"display"=>true}}
-    }
-    let(:expected_value) {[
-      {"display"=>false, "title"=>"Homepage", "url"=>"/homepage"},
-      {"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
-    ]}
+    let(:website_value) {{
+        "1"=>{"display"=>true, "title"=>"Homepage", "url"=>"/homepage"},
+        "2"=>{"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
+      }}
+
+    let(:widget_value) {{
+      "1"=>{"display"=>false},
+      "2"=>{"display"=>true}
+      }}
+
+    let(:expected_value) {{
+        "1"=>{"display"=>false, "title"=>"Homepage", "url"=>"/homepage"},
+        "2"=>{"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
+      }}
 
     it "Keeps new display value" do
       described_instance.create_new_value(website_value, widget_value).should eq expected_value
@@ -22,17 +25,17 @@ shared_examples_for SettingNavigation do
   end
 
   describe "When the title changes on website" do
-    let(:widget_value) {[
-        {"display"=>false, "title"=>"Old Title", "url"=>"/old-title"},
-      ]}
-    let(:website_value) {[
-        {"display"=>true, "title"=>"New Title", "url"=>"/new-title"},
-        {"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
-      ]}
-    let(:expected_value) {[
-      {"display"=>false, "title"=>"New Title", "url"=>"/new-title"},
-      {"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
-    ]}
+    let(:widget_value) {{
+        "1"=>{"display"=>false, "title"=>"Old Title", "url"=>"/old-title"},
+      }}
+    let(:website_value) {{
+        "1"=>{"display"=>true, "title"=>"New Title", "url"=>"/new-title"},
+        "2"=>{"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
+      }}
+    let(:expected_value) {{
+      "1"=>{"display"=>false, "title"=>"New Title", "url"=>"/new-title"},
+      "2"=>{"display"=>true, "title"=>"New Page", "url"=>"/new-page"}
+      }}
 
     it "Keeps new title and old display value" do
       described_instance.create_new_value(website_value, widget_value).should eq expected_value
