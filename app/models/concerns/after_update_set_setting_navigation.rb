703d575022d3633bd4614b1894da5bea94abcaa4
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-12-10 16:35:16 -0800

    Update website navigation setting when web template display order changes

--- app/models/concerns/after_update_set_setting_navigation.rb
+++ app/models/concerns/after_update_set_setting_navigation.rb
@@ -3,12 +3,23 @@ module AfterUpdateSetSettingNavigation
 
   included do
     after_create :update_navigation_settings
-    after_update :update_navigation_settings, if: :name_changed?
+    after_update :update_home_page_template_display_order, if: :display_order_changed?
+    after_update :update_navigation_settings, if: :should_update_navigation_settings?
   end
 
   private
 
+  def should_update_navigation_settings?
+    name_changed? || display_order_changed?
+  end
+
   def update_navigation_settings
     website.try(:update_navigation_settings)
+  end
+
+  def update_home_page_template_display_order
+    if !web_home_template? && website && website.web_home_template
+      website.web_home_template.update_attribute(:display_order, -9999999)
+    end
   end
 end
