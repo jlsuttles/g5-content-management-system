00d2a9df9d7b2a5f317cc84a6c9933d4bad759bf
jessicard <jessicard@mac.com>
2013-12-11 18:25:36 -0800

    adds vertical, city & state to fabricators

--- spec/fabricators/location_fabricator.rb
+++ spec/fabricators/location_fabricator.rb
@@ -2,4 +2,6 @@ Fabricator :location do
   uid { Faker::Internet.url }
   urn { "g5-cl-2398223-#{Faker::Name.name.parameterize}" }
   name { Faker::Name.name }
+  state { Faker::Address.state_abbr }
+  city { Faker::Address.city }
 end
