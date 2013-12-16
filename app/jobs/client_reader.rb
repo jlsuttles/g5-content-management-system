51d685e834be0b7fc2795104d013bfef0eb9223b
jessicard <jessicard@mac.com>
2013-12-16 14:36:55 -0800

    update client & location in client_reader instead of destroying

--- app/jobs/client_reader.rb
+++ app/jobs/client_reader.rb
@@ -4,6 +4,7 @@ class ClientReader
 
   def self.perform(client_uid)
     clients = Microformats2.parse(client_uid)
+<<<<<<< HEAD
     client = clients.first
 
     Client.destroy_all
@@ -26,5 +27,30 @@ class ClientReader
         postal_code: location.adr.try(:format).try(:postal_code).to_s
       )
     end if client.respond_to?(:orgs)
+=======
+    first_client = clients.first
+
+    client = Client.find_or_create_by_uid(uid: client_uid)
+
+    client.uid = client_uid
+    client.name = first_client.name.to_s
+    client.vertical = first_client.g5_vertical.to_s
+
+    client.save
+
+    first_client.orgs.each do |org|
+      org = org.format
+
+      location = Location.find_or_create_by_uid(uid: org.uid.to_s)
+
+      location.uid = org.uid.to_s
+      location.urn = org.uid.to_s.split("/").last
+      location.name = org.name.to_s
+      location.state = org.adr.try(:format).try(:region).to_s
+      location.city = org.adr.try(:format).try(:locality).to_s
+
+      location.save
+    end if first_client.respond_to?(:orgs)
+>>>>>>> update client & location in client_reader instead of destroying
   end
 end
