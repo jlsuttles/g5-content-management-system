9715b1e6a89c62e4820ed839c011ed8a506588cb
jessicard <jessicard@mac.com>
2013-12-18 18:13:03 -0800

    fixes missed comma

--- lib/static_website/compiler/htaccess.rb
+++ lib/static_website/compiler/htaccess.rb
@@ -40,7 +40,7 @@ module StaticWebsite
 
         htaccess_contents = ["<IfModule mod_rewrite.c>",
                             "\tRewriteEngine On",
-                            "\tRewriteRule ^#{pattern} #{template.htaccess_substitution} [R=301,L]"
+                            "\tRewriteRule ^#{pattern} #{template.htaccess_substitution} [R=301,L]",
                             empty_folders,
                             redirect_rules,
                             "\tRewriteCond %{REQUEST_FILENAME} !-d",
