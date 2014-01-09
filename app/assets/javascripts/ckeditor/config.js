e76168454797bf3a4c08248b716d63aeda1f670a
Chad Crissman <cdcrissman@gmail.com>
2014-01-09 15:15:35 -0500

    adds image button to html widget text editor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,8 +3,9 @@ CKEDITOR.config.toolbarGroups = [
 	{ name: 'basicstyles' },
 	{ name: 'paragraph', groups: [ 'list', 'align' ] },
 	{ name: 'links' },
+  { name: 'insert' },
 	{ name: 'styles' }
 ];
-CKEDITOR.config.removeButtons = 'Anchor,Underline,Strike,Subscript,Superscript,Copy,Cut,Styles,Font,FontSize';
+CKEDITOR.config.removeButtons = 'Anchor,Underline,Strike,Subscript,Superscript,Copy,Cut,Styles,Font,FontSize,Flash,Table,Smiley,SpecialChar,PageBreak,Iframe';
 CKEDITOR.config.removeDialogTabs = 'link:advanced';
 CKEDITOR.config.dialog_backgroundCoverColor = 'transparent'
