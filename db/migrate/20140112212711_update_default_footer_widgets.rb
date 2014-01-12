3dedc2f40b25f41aff5aed92816b5325d99c4240
Chad Crissman <cdcrissman@gmail.com>
2014-01-12 16:32:00 -0500

    changes default footer widgets

---
+++ db/migrate/20140112212711_update_default_footer_widgets.rb
@@ -0,0 +1,12 @@
+class UpdateDefaultFooterWidgets < ActiveRecord::Migration
+  def up
+    DropTarget.where(html_id: “drop-target-footer”).each do |footer|
+      footer.widgets.build(url: Widget.component_url(“footer-info”))
+      footer.widgets.where(url: Widget.component_url(“photo”)).destroy_all
+      footer.save
+   end
+  end
+
+  def down
+  end
+end
