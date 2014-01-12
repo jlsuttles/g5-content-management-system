3d622df94ceccabf7570c0596bb5b248fca01e70
Chad Crissman <cdcrissman@gmail.com>
2014-01-12 17:37:19 -0500

    fixes migration error

--- db/migrate/20140112212711_update_default_footer_widgets.rb
+++ db/migrate/20140112212711_update_default_footer_widgets.rb
@@ -1,8 +1,8 @@
 class UpdateDefaultFooterWidgets < ActiveRecord::Migration
   def up
-    DropTarget.where(html_id: “drop-target-footer”).each do |footer|
-      footer.widgets.build(url: Widget.component_url(“footer-info”))
-      footer.widgets.where(url: Widget.component_url(“photo”)).destroy_all
+    DropTarget.where(html_id: "drop-target-footer").each do |footer|
+      footer.widgets.build(url: Widget.component_url("footer-info"))
+      footer.widgets.where(url: Widget.component_url("photo")).destroy_all
       footer.save
    end
   end
