40b045b0c11f3401c201a2514112faf9e7a7b8ee
Chad Crissman <cdcrissman@gmail.com>
2014-01-16 16:26:55 -0500

    add leads service js to client pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
     end
     image_tag(src, options)
   end
+  
+  def leads_service_js
+    url = @client.urn.gsub("-c-", "-cls-")
+    domain = "herokuapp.com"
+    "http://#{url}.#{domain}/assets/form_enhancer.js"
+  end
 end
