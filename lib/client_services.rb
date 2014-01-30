e60795f78b984e6131bd5315a0be0eaab30925bb
jessicard <jessicard@mac.com>
2014-01-30 13:10:57 -0800

    programmatically generate service methods

--- lib/client_services.rb
+++ lib/client_services.rb
@@ -8,47 +8,35 @@ class ClientServices
   #client leads service
   #client experience management?
 
-  # client_urn
   # client_location_urn
-  # client_cms_urn
-  # client_leads_urn
-  # client_pricing_availability_urn
-  # client_phone_numbers_urn
-  # client_cxm_urn
-
-  # client_url
   # client_location_url
-  # client_cms_url
-  # client_leads_url
-  # client_pricing_availability_url
-  # client_phone_numbers_url
-  # client_cxm_url
 
   def client_urn
     Client.first.urn
   end
 
-  # TODO: Programatically generate these methods using SERVICES
-  #
-  # SERVICES.each do |service|
-  #   define_method("#{service}_urn") do
-  #     ...
-  #   end
-  #   ...
-  # end
-
-  def cpns_urn
-    # Custom or replace the Client's app prefix
-    ENV["CPNS_URN"] || client_urn.gsub(/-c-/, "-cpns-")
+  def client_app_name
+    client_urn[0...HEROKU_APP_NAME_MAX_LENGTH]
   end
 
-  def cpns_app_name
-    # Custom or truncate to Heroku's max app name length
-    ENV["CPNS_APP_NAME"] || cpns_urn[0...HEROKU_APP_NAME_MAX_LENGTH]
+  def client_url
+    "http://#{client_app_name}.herokuapp.com/"
   end
 
-  def cpns_url
-    # Custom or Heroku URL
-    ENV["CPNS_URN"] || "http://#{cpns_app_name}.herokuapp.com/"
+  SERVICES.each do |service|
+    define_method("#{service}_urn") do
+      # Custom or replace the Client's app prefix
+      ENV["#{service}_URN"] || client_urn.gsub(/-c-/, "-#{service}-")
+    end
+
+    define_method("#{service}_app_name") do
+      # Custom or truncate to Heroku's max app name length
+      ENV["#{service}_APP_NAME"] || send("#{service}_urn")[0...HEROKU_APP_NAME_MAX_LENGTH]
+    end
+
+    define_method("#{service}_url") do
+      # Custom or Heroku URL
+      ENV["#{service}_URL"] || ("http://" + send("#{service}_app_name") + ".herokuapp.com/")
+    end
   end
 end
