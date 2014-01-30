dcfdd9e7967491854c8a715099200e32da5cbac6
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-01-30 13:36:27 -0800

    Add phone number to locations on start up

--- lib/client_reader.rb
+++ lib/client_reader.rb
@@ -44,11 +44,14 @@ class ClientReader
       # the database or create a new one.
 
       location = Location.find_or_initialize_by_uid(uid: uf2_location.uid.to_s)
-      location.urn    = uf2_location.uid.to_s.split("/").last
-      location.name   = uf2_location.name.to_s
-      location.domain = uf2_location.g5_domain.to_s
-      location.state  = uf2_location.adr.try(:format).try(:region).to_s
-      location.city   = uf2_location.adr.try(:format).try(:locality).to_s
+      location.urn            = uf2_location.uid.to_s.split("/").last
+      location.name           = uf2_location.name.to_s
+      location.domain         = uf2_location.g5_domain.to_s
+      location.street_address = uf2_location.adr.try(:format).try(:street_address).to_s
+      location.state          = uf2_location.adr.try(:format).try(:region).to_s
+      location.city           = uf2_location.adr.try(:format).try(:locality).to_s
+      location.postal_code    = uf2_location.adr.try(:format).try(:postal_code).to_s
+      location.phone_number   = uf2_location.adr.try(:format).try(:tel).to_s
 
       location.save
     end if uf2_client.respond_to?(:orgs)
