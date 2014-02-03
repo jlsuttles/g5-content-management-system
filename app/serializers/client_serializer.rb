4a4815ac8db55abf112d0c8c8adf642d54d8e821
jessicard <jessicard@mac.com>
2014-02-03 12:14:45 -0800

    adds ClientServices methods to the ClientSerializer & client ember model

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -5,5 +5,58 @@ class ClientSerializer < ActiveModel::Serializer
 
   attributes  :id,
               :urn,
-              :name
+              :name,
+              :app_name,
+              :url,
+              :location_urns,
+              :location_urls,
+              :cms_urn,
+              :cms_app_name,
+              :cms_url,
+              :cpns_urn,
+              :cpns_app_name,
+              :cpns_url,
+              :cpas_urn,
+              :cpas_app_name,
+              :cpas_url,
+              :cls_urn,
+              :cls_app_name,
+              :cls_url,
+              :cxm_urn,
+              :cxm_app_name,
+              :cxm_url
+
+  def client_services
+    ClientServices.new
+  end
+
+  def app_name
+    client_services.client_app_name
+  end
+
+  def url
+    client_services.client_url
+  end
+
+  def location_urns
+    client_services.client_location_urns
+  end
+
+  def location_urls
+    client_services.client_location_urls
+  end
+
+  ClientServices::SERVICES.each do |service|
+    define_method("#{service}_urn") do
+      client_services.send(:"#{service}_urn")
+    end
+
+    define_method("#{service}_app_name") do
+      client_services.send(:"#{service}_app_name")
+    end
+
+    define_method("#{service}_url") do
+      client_services.send(:"#{service}_url")
+    end
+  end
 end
