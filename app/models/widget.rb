e1fa08bd7193194eab55204c0f2bc3041f000721
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-02-10 13:48:05 -0800

    Do not display Meta Description Widget in .drop-target-main

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -23,6 +23,7 @@ class Widget < ActiveRecord::Base
   validates :url, presence: true
 
   scope :name_like_form, where("widgets.name LIKE '%Form'")
+  scope :not_meta_description, where("widgets.name != ?", "Meta Description")
 
   def website_id
     web_template.website_id if web_template
