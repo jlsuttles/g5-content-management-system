dd47df9120367b55ba1b3fdc83ed9b662f2bc982
Chad Crissman <cdcrissman@gmail.com>
2014-02-13 15:34:18 -0500

    migration for updating footer info widget

---
+++ db/migrate/20140213203328_update_footer_info_again.rb
@@ -0,0 +1,12 @@
+class UpdateFooterInfoAgain < ActiveRecord::Migration
+  def up
+    DropTarget.where(html_id: "drop-target-footer").each do |footer|
+      footer.widgets.where(url: Widget.component_url("footer-info")).destroy_all
+      footer.widgets.build(url: Widget.component_url("footer-info"))
+      footer.save
+    end
+  end
+
+  def down
+  end
+end
