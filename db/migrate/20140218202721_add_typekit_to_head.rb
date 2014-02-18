96abbfa83943e5935d5a45b0e06930097bbd4edf
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-02-18 13:08:27 -0800

    Update 20140218202721_add_typekit_to_head.rb

--- db/migrate/20140218202721_add_typekit_to_head.rb
+++ db/migrate/20140218202721_add_typekit_to_head.rb
@@ -1,7 +1,12 @@
 class AddTypekitToHead < ActiveRecord::Migration
   def change
+    # Make sure the typekit widget has been added to the database
+    GarenWidgetUpdater.new.update_all
+    # Find the GardenWidget (These are like the remote wigets, but they aren't remote anymore)
+    garden_widget = GardenWidget.where(url: GardenWidget.component_url("typekit"))
+    # Create the new widgets in the head
     DropTarget.where(html_id: "drop-target-head").each do |head|
-      head.widgets.build(url: Widget.component_url("typekit"))
+      head.widgets.build(garden_widget_id: garden_widget.id)
       head.save
    end
   end
