a9d6533ba3f38d36a2aef55d16c11c5865b7c3de
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-02-18 13:10:50 -0800

    Update 20140218202721_add_typekit_to_head.rb
    Fix typo.

--- db/migrate/20140218202721_add_typekit_to_head.rb
+++ db/migrate/20140218202721_add_typekit_to_head.rb
@@ -1,7 +1,7 @@
 class AddTypekitToHead < ActiveRecord::Migration
   def change
     # Make sure the typekit widget has been added to the database
-    GarenWidgetUpdater.new.update_all
+    GardenWidgetUpdater.new.update_all
     # Find the GardenWidget (These are like the remote wigets, but they aren't remote anymore)
     garden_widget = GardenWidget.where(url: GardenWidget.component_url("typekit"))
     # Create the new widgets in the head
