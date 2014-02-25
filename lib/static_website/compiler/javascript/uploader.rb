b3ef196a1f9d6aec6e44433951cc545b02b9c217
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-02-25 03:47:15 -0800

    Specify AWS region

--- lib/static_website/compiler/javascript/uploader.rb
+++ lib/static_website/compiler/javascript/uploader.rb
@@ -10,7 +10,8 @@ module StaticWebsite
           @from_paths = from_paths
           @s3 = AWS::S3.new(
             access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-            secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+            secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+            region: ENV["AWS_REGION"] || "us-west-2"
           )
           location_name = location_name.to_s.underscore.upcase
           @bucket_name = ENV["AWS_S3_BUCKET_NAME_#{location_name}"]
