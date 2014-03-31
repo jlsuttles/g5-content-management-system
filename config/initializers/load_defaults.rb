b0daa59898d2e2fadddadecaa5959f65e8d3de0b
Chad Crissman <cdcrissman@gmail.com>
2014-03-31 16:59:09 -0400

    creates a defaults config for each vertical

--- config/initializers/load_defaults.rb
+++ config/initializers/load_defaults.rb
@@ -1,1 +1,16 @@
-DEFAULTS = YAML.load_file("#{Rails.root}/config/defaults.yml")
+case Client.first.vertical
+
+when 'Assisted-Living'
+  default_template = 'defaults_sl.yml'
+
+when 'Self-Storage'
+  default_template = 'defaults_ss.yml'
+
+when 'Apartments'
+  default_template = 'defaults_mf.yml'
+
+else
+  default_template = 'defaults.yml'
+end
+
+DEFAULTS = YAML.load_file("#{Rails.root}/config/#{default_template}")
