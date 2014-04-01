f0c4bec5508aba5a68dc202d7f93194e7d8e6024
Chad Crissman <cdcrissman@gmail.com>
2014-04-01 13:01:21 -0400

    use conditional to set vertical specific defaults

--- lib/website_seeder.rb
+++ lib/website_seeder.rb
@@ -2,15 +2,13 @@ class WebsiteSeeder
   attr_reader :location, :instructions, :website
 
   def initialize(location, instructions=DEFAULTS["website"])
-    case Client.first.vertical
-    when 'Assisted-Living'
+
+    if Client.first.vertical == 'Assisted-Living'
       instructions = ASSISTED_LIVING_DEFAULTS["website"]
-    when 'Self-Storage'
+    elsif Client.first.vertical == 'Self-Storage'
       instructions = SELF_STORAGE_DEFAULTS["website"]
-    when 'Apartments'
+    elsif Client.first.vertical == 'Apartments'
       instructions = APARTMENTS_DEFAULTS["website"]
-    else
-      instructions = DEFAULTS["website"]
     end
 
     @location = location
