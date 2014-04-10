314e381f8da72f805749ce4e5e147671fa9199e2
bbauer <brian.bauer@g5platform.com>
2014-04-10 13:12:52 -0700

    Remove un-used controller

--- app/controllers/releases_controller.rb
+++
@@ -1,6 +0,0 @@
-class ReleasesController < ApplicationController
-  def rollback
-    #ReleasesManager.new(params["website_slug"]).rollback(params[:id])
-    redirect_to root_path, notice: "Rolling Back #{params["location_slug"]} To #{params[:rollback_id]}. This may take a few minutes."
-  end
-end
