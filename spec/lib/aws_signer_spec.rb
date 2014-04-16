d7cd6757ba9b3a692c462df6666c712d2abf741b
Michael Mitchell <michael.mitchell@getg5.com>
2014-04-16 10:46:09 -0700

    adds specs for each public method of aws_signer

--- spec/lib/aws_signer_spec.rb
+++ spec/lib/aws_signer_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe AWSSigner do
-  
+  subject do
+    AWSSigner.new({'locationName' => 'foobarlocation'})
+  end
+
+  describe "#upload_headers" do
+    it "returns a hash of header items" do
+      subject.upload_headers.should include :acl => "public-read"
+    end
+  end  
+
+  describe "#delete_headers" do
+    it "returns a hash of header items" do
+      subject.delete_headers.should include :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID']
+    end
+  end  
 end
+
