61c2c0461ca7cf47fa6696316f3bdf6aaef9be1e
bbauer <brian.bauer@g5platform.com>
2014-04-21 16:16:26 -0700

    Backfill more WebsiteSeeder specs

--- spec/lib/website_seeder_spec.rb
+++ spec/lib/website_seeder_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe WebsiteSeeder do
   let!(:client) { Fabricate(:client) }
   let!(:location) { Fabricate(:location) }
+  let(:website) { Fabricate(:website) }
+  let(:defaults) { YAML.load_file("#{Rails.root}/config/defaults.yml")["website"] }
   let(:seeder) { WebsiteSeeder.new(location) }
 
   def setting_value_for(name)
@@ -10,13 +12,19 @@ describe WebsiteSeeder do
   end
 
   describe "#seed" do
-    let(:website) { Fabricate(:website) }
     let(:client_services) { ClientServices.new }
 
     before { location.stub(create_website: website) }
     subject { seeder.seed }
 
-    it { should eq(website) }
+    it "creates a website for the location" do
+      location.should_receive(:create_website).and_return(website)
+      subject
+    end
+
+    it "returns a website" do
+      expect(subject).to eq(website)
+    end
 
     describe "setting creation" do
       before { subject }
@@ -34,6 +42,160 @@ describe WebsiteSeeder do
         expect(setting_value_for("phone_number")).to eq(location.phone_number)
         expect(setting_value_for("available_garden_widgets")).to eq(GardenWidgetsSetting.new.value)
       end
+    end
+
+    describe "template creation" do
+      after { subject }
+
+      it "creates a website template" do
+        seeder.should_receive(:create_website_template)
+      end
+
+      it "creates a web home template" do
+        seeder.should_receive(:create_web_home_template)
+      end
+
+      it "creates a web page template" do
+        seeder.should_receive(:create_web_page_templates)
+      end
+    end
+  end
+
+  describe "#create_website_template" do
+    let(:instructions) { defaults["website_template"] }
+    let!(:website_template) { Fabricate(:website_template) }
+
+    subject { seeder.create_website_template(website, instructions) }
+
+    context "a valid website" do
+      before { website.stub(create_website_template: website_template) }
+      after  { subject }
+
+      it "creates a website template for the website" do
+        website.should_receive(:create_website_template)
+      end
+
+      it "creates a web layout for the template" do
+        website_template.should_receive(:create_web_layout)
+      end
+
+      it "creates a web theme for the template" do
+        website_template.should_receive(:create_web_theme)
+      end
+
+      it "creates drop targets" do
+        seeder.should_receive(:create_drop_targets).with(website_template, instructions["drop_targets"])
+      end
+    end
+
+    context "no website" do
+      let(:website) { nil }
+
+      it { should be_nil }
+    end
+  end
+
+  describe "#create_web_home_template" do
+    let(:instructions) { defaults["web_home_template"] }
+    let!(:web_home_template) { Fabricate(:web_home_template) }
+
+    subject { seeder.create_web_home_template(website, instructions) }
+
+    context "a valid website" do
+      before { website.stub(create_web_home_template: web_home_template) }
+      after  { subject }
+
+      it "creates a web home template for the website" do
+        website.should_receive(:create_web_home_template)
+      end
+
+      it "creates drop targets" do
+        seeder.should_receive(:create_drop_targets).with(web_home_template, instructions["drop_targets"])
+      end
+    end
+
+    context "no website" do
+      let(:website) { nil }
+
+      it { should be_nil }
+    end
+  end
+
+  describe "#create_web_page_templates" do
+    let(:instructions) { [defaults["web_page_templates"].first] }
+    let!(:web_page_template) { Fabricate(:web_page_template) }
+
+    subject { seeder.create_web_page_templates(website, instructions) }
+
+    context "a valid website" do
+      before { website.web_page_templates.stub(create: web_page_template) }
+      after  { subject }
+
+      it "creates a web page template for each instruction" do
+        website.web_page_templates.should_receive(:create)
+      end
+
+      it "creates drop targets" do
+        seeder.should_receive(:create_drop_targets).
+          with(web_page_template, instructions.first["drop_targets"])
+      end
+    end
+
+    context "no website" do
+      let(:website) { nil }
+
+      it { should be_nil }
+    end
+  end
+
+  describe "#create_drop_targets" do
+    let!(:web_template) { Fabricate(:website_template) }
+    let!(:drop_target) { Fabricate(:drop_target) }
+    let(:instructions) { defaults["web_page_templates"].first["drop_targets"] }
+
+    subject { seeder.create_drop_targets(web_template, instructions) }
+
+    context "a valid website" do
+      before { web_template.drop_targets.stub(create: drop_target) }
+      after  { subject }
+
+      it "creates a drop template for each instruction" do
+        web_template.drop_targets.should_receive(:create)
+      end
+
+      it "creates drop targets" do
+        seeder.should_receive(:create_widgets).
+          with(drop_target, instructions.first["widgets"])
+      end
+    end
+
+    context "no web template" do
+      let(:web_template) { nil }
+
+      it { should be_nil }
+    end
+  end
+
+  describe "#create_widgets" do
+    let(:instructions) { defaults["web_page_templates"].first["drop_targets"].first["widgets"] }
+    let(:drop_target) { Fabricate.build(:drop_target) }
+    let(:widget) { Fabricate.build(:widget) }
+
+    subject { seeder.create_widgets(drop_target, instructions) }
+
+    context "a valid website" do
+      before { drop_target.widgets.stub(create: widget) }
+      after  { subject }
+
+      it "creates a drop template for each instruction" do
+        drop_target.widgets.should_receive(:create)
+      end
+    end
+
+    context "no web template" do
+      let(:drop_target) { nil }
+
+      it { should be_nil }
     end
   end
 end
