68427630687cdfe7bf0c360bbe7626b62a868e92
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-04-23 10:08:27 -0700

    Move garden urls to .env

--- config/initializers/env.rb
+++
@@ -1,3 +0,0 @@
-ENV["WIDGET_GARDEN_URL"] ||= "http://g5-widget-garden.herokuapp.com"
-ENV["THEME_GARDEN_URL"] ||= "http://g5-theme-garden.herokuapp.com"
-ENV["LAYOUT_GARDEN_URL"] ||= "http://g5-layout-garden.herokuapp.com"
