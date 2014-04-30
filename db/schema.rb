0d9a4e51c238bcd7c42b6dbdafd0f99263463afe
bbauer <brian.bauer@g5platform.com>
2014-04-30 10:02:46 -0700

    Assign client as MultiDomainClient
    All clients will be multi domain clients when this is deployed and needs to be
    set correctly.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140429231417) do
+ActiveRecord::Schema.define(:version => 20140430165903) do
 
   create_table "assets", :force => true do |t|
     t.string   "url"
