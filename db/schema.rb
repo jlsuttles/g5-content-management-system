7cd9bec77b36d53e4a3b773e4a272639176fdefb
bbauer <brian.bauer@g5platform.com>
2014-05-12 12:58:09 -0700

    Add domain to client

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506195829) do
+ActiveRecord::Schema.define(version: 20140512193842) do
 
   create_table "assets", force: true do |t|
     t.string   "url"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140506195829) do
     t.datetime "updated_at"
     t.string   "vertical"
     t.string   "type"
+    t.string   "domain"
   end
 
   create_table "drop_targets", force: true do |t|
