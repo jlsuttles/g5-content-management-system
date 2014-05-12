53b7d03891350852583a687c2776b18025a064c7
bbauer <brian.bauer@g5platform.com>
2014-05-12 14:46:31 -0700

    Add the actual deploy action to controller

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::ClientsController < Api::V1::ApplicationController
   def show
     render json: Client.first, serializer: ClientSerializer
   end
+
+  def deploy_websites
+    Client.first.async_deploy
+    redirect_to root_path, notice: "Deploying websites. This may take few minutes."
+  end
 end
