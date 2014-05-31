de566ec44c785fc797ba6ac231679e73188b060e
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-05-31 14:30:03 -0700

    Fix spec
    Rewrite test to be simpler and work

--- spec/models/concerns/has_setting_locations_navigation_spec.rb
+++ spec/models/concerns/has_setting_locations_navigation_spec.rb
@@ -4,13 +4,15 @@ shared_examples_for HasSettingLocationsNavigation do
   let(:described_instance) { described_class.new }
 
   describe "#update_locations_navigation_setting" do
-    before { Location.any_instance.stub(:update_locations_navigation_settings) }
     before { described_instance.save(validate: false) }
+    before { Location.any_instance.stub(:update_locations_navigation_settings) }
     let!(:location) { Fabricate(:location) }
     let!(:setting) { Fabricate(:setting, owner: described_instance, name: "locations_navigation") }
 
     it "updates settings" do
-      expect { described_instance.update_locations_navigation_setting }.to change(setting, :value)
+      expect(setting.value).to be_nil
+      described_instance.update_locations_navigation_setting
+      expect(setting.reload.value).to_not be_nil
     end
   end
 end
