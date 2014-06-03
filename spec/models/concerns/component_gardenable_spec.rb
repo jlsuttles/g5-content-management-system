903e84aafb9f565c649b73ef0aeb9e281e9ccd0e
Michael Mitchell <michael.mitchell@getg5.com>
2014-06-03 16:12:31 -0700

    accepts alert dialog - fixes spec

--- spec/models/concerns/component_gardenable_spec.rb
+++ spec/models/concerns/component_gardenable_spec.rb
@@ -34,7 +34,7 @@ describe ComponentGardenable, vcr: { record: :new_episodes } do
           stub_const "MAIN_APP_UID", 'foo'
         end
         it "should reject components when they have targets not including our UID" do
-          Component.components_microformats.length.should == 41
+          Component.components_microformats.length.should == 40
         end
       end
 
