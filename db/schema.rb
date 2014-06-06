0ee2b1fdf863c00c3eec7f243f375928c264c258
bbauer <brian.bauer@g5platform.com>
2014-06-06 12:13:37 -0700

    Add setting for column widget

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521185630) do
+ActiveRecord::Schema.define(version: 20140606191126) do
 
   create_table "assets", force: true do |t|
     t.string   "url"
