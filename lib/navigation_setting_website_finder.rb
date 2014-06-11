a010bffdf1185cf38c56b127e220713c9806ec5f
bbauer <brian.bauer@g5platform.com>
2014-06-11 15:07:48 -0700

    Simplify website lookup

--- lib/navigation_setting_website_finder.rb
+++ lib/navigation_setting_website_finder.rb
@@ -1,50 +1,27 @@
 class NavigationSettingWebsiteFinder
   def initialize(setting)
     @setting = setting
+    @widget = @setting.owner
+    @website = nil
   end
 
   def find
-    return unless parent_widget
-    return drop_target.web_template.website if drop_target
-    return parent_widget_drop_target.web_template.website if parent_widget_drop_target
+    loop do
+      setting = find_setting(@widget.id)
 
-    parent_widget_parent_widget_website
+      return unless setting
+
+      website_for(setting.owner)
+
+      return @website unless @website.nil?
+    end
   end
 
 private
 
-  def parent_setting
-    @parent_setting ||= find_setting(@setting.owner.id)
-  end
-
-  def parent_widget
-    @parent_widget ||= parent_setting.owner if parent_setting
-  end
-
-  def drop_target
-    @drop_target ||= parent_widget.drop_target
-  end
-
-  def parent_widget_setting
-    @parent_widget_setting ||= find_setting(parent_widget.id)
-  end
-
-  def parent_widget_parent_widget
-    @parent_widget_parent_widget ||= parent_widget_setting.owner
-  end
-
-  def parent_widget_drop_target
-    @parent_widget_drop_target ||= parent_widget_parent_widget.drop_target
-  end
-
-  def parent_widget_parent_setting
-    @parent_widget_parent_setting ||= find_setting(parent_widget_parent_widget.id)
-  end
-
-  def parent_widget_parent_widget_website
-    if parent_widget_parent_setting
-      parent_widget_parent_setting.owner.drop_target.web_template.website
-    end
+  def website_for(widget)
+    @widget = widget
+    @website = @widget.drop_target.web_template.website if @widget.drop_target
   end
 
   def find_setting(widget_id)
