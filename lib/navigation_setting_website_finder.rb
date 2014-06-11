6203a2ca82ebffc4b6429fd41dd8cefaf5a5d759
bbauer <brian.bauer@g5platform.com>
2014-06-11 14:33:41 -0700

    Support for three level deep parent widgets
    This needs some refactoring love...

--- lib/navigation_setting_website_finder.rb
+++ lib/navigation_setting_website_finder.rb
@@ -6,26 +6,45 @@ class NavigationSettingWebsiteFinder
   def find
     return unless parent_widget
     return drop_target.web_template.website if drop_target
+    return parent_widget_drop_target.web_template.website if parent_widget_drop_target
 
-    parent_widget_parent_website
+    parent_widget_parent_widget_website
   end
 
 private
 
-  def parent_widget
-    @parent_widget ||= parent_setting.owner if parent_setting
-  end
-
   def parent_setting
     @parent_setting ||= find_setting(@setting.owner.id)
+  end
+
+  def parent_widget
+    @parent_widget ||= parent_setting.owner if parent_setting
   end
 
   def drop_target
     @drop_target ||= parent_widget.drop_target
   end
 
-  def parent_widget_parent_website
-    find_setting(parent_widget.id).owner.drop_target.web_template.website
+  def parent_widget_setting
+    @parent_widget_setting ||= find_setting(parent_widget.id)
+  end
+
+  def parent_widget_parent_widget
+    @parent_widget_parent_widget ||= parent_widget_setting.owner
+  end
+
+  def parent_widget_drop_target
+    @parent_widget_drop_target ||= parent_widget_parent_widget.drop_target
+  end
+
+  def parent_widget_parent_setting
+    @parent_widget_parent_setting ||= find_setting(parent_widget_parent_widget.id)
+  end
+
+  def parent_widget_parent_widget_website
+    if parent_widget_parent_setting
+      parent_widget_parent_setting.owner.drop_target.web_template.website
+    end
   end
 
   def find_setting(widget_id)
