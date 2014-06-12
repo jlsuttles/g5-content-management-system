4e886395f2e1b07c7dd34e834e117a6222f6ff2a
Michael Mitchell <michael.mitchell@getg5.com>
2014-06-12 15:23:38 -0700

    adds specs for new widget instance methods

--- spec/fabricators/garden_widget_fabricator.rb
+++ spec/fabricators/garden_widget_fabricator.rb
@@ -5,4 +5,7 @@ Fabricator :garden_widget do
   thumbnail { Faker::Internet.url }
   edit_html { "<div>edit</div>" }
   show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
+  show_stylesheets {["foo.css", "bar.css"]}
+  show_javascript {"show.js"}
+  lib_javascripts {["a.js", "b.js"]}
 end
